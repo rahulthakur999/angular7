4a005cb155459b6a109e40e8258dcfe13004f816
rahulthakur999@gmail.com <rahul.kumar@ordergenie.co.in>
2019-03-19 18:56:48 +0530

    trackby page done

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import{Routes,RouterModule} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { TrackbyComponent } from './trackby/trackby.component';
 
 const routes:Routes = [
   {
     path:'home', 
     component:HomeComponent
   },
+  {path:'trackby',component:TrackbyComponent},
   {
     path:'aboutus', 
     component:AboutusComponent
--- src/app/trackby/trackby.component.ts
+++ src/app/trackby/trackby.component.ts
@@ -9,6 +9,30 @@ export class TrackbyComponent implements OnInit {
 
   constructor() { }
 
+  data=[
+    {id:1,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+    {id:2,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+    {id:3,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+    {id:4,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+    {id:5,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+  ];
+
+  addmore(){
+    this.data=[
+      {id:1,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+      {id:2,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+      {id:3,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+      {id:4,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+      {id:5,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+      {id:6,name:'Mohal',email:'mohan.kumar@ordergenie.co.in'},
+    ]; 
+  }
+
+  trackbyid(index:number,name:any):string{
+     console.log(name.id);
+     return name.id;
+  }
+
   ngOnInit() {
   }
 
