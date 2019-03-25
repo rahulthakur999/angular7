c4fd361840ce0b5f44b0d94bb61afa95e50620a4
rahulthakur999@gmail.com <rahul.kumar@ordergenie.co.in>
2019-03-25 19:03:28 +0530

    added sort and find

--- src/app/trackby/trackby.component.ts
+++ src/app/trackby/trackby.component.ts
@@ -9,13 +9,20 @@ export class TrackbyComponent implements OnInit {
 
   constructor() { }
 
+  arr=['Rahul', 'Amit', 'Mohan'];
+  arrnum=[3,5,1,6,8,7,9,23,12,6];
+  know:number;
+
   data=[
-    {id:1,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
-    {id:2,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
-    {id:3,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
-    {id:4,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
-    {id:5,name:'Rahul',email:'rahul.kumar@ordergenie.co.in'},
+    {id:5,name:'Rahul 5',email:'rahul.kumar@ordergenie.co.in'},
+    {id:1,name:'Rahul 1',email:'rahul.kumar@ordergenie.co.in'},
+    {id:2,name:'Rahul 2',email:'rahul.kumar@ordergenie.co.in'},
+    {id:3,name:'Rahul 3',email:'rahul.kumar@ordergenie.co.in'},
+    {id:4,name:'Rahul 4',email:'rahul.kumar@ordergenie.co.in'}, 
   ];
+
+   
+  
 
   addmore(){
     this.data=[
@@ -29,11 +36,20 @@ export class TrackbyComponent implements OnInit {
   }
 
   trackbyid(index:number,name:any):string{
-     console.log(name.id);
+    // console.log(name.id);
      return name.id;
   }
 
   ngOnInit() {
+
+    this.arr=this.arr.sort(); 
+    console.log('arr.Sort(): ',this.arr);
+
+    this.know = this.arrnum.find(function(e){
+      return e > 6 && e < 12 ;
+    })
+    console.log('arr.find(): ',this.know);
+
   }
 
 }
